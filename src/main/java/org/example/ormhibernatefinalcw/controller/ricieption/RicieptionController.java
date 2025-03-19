1054d1ebb427c9d5dc4c9d377740e019d0591747
ireshdilsh <ireshdilshan46@gmail.com>
2025-03-19 12:10:59 +0530

    update

--- src/main/java/org/example/ormhibernatefinalcw/controller/ricieption/RicieptionController.java
+++ src/main/java/org/example/ormhibernatefinalcw/controller/ricieption/RicieptionController.java
@@ -1,4 +1,17 @@
 package org.example.ormhibernatefinalcw.controller.ricieption;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.layout.AnchorPane;
+import org.example.ormhibernatefinalcw.utils.WindowUtils;
+
 public class RicieptionController {
+
+    @FXML
+    private AnchorPane loginPane;
+    
+    @FXML
+    void gotoLandingPage(ActionEvent event) throws Exception{
+        new WindowUtils().navigateTo("Landing",loginPane);
+    }
 }
