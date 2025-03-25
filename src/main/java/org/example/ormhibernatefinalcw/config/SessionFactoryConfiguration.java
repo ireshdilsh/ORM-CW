f709aa6aba1416275bba6a83a2186e9a84c41ced
ireshdilsh <ireshdilshan46@gmail.com>
2025-03-25 10:16:49 +0530

    updated

--- src/main/java/org/example/ormhibernatefinalcw/config/SessionFactoryConfiguration.java
+++ src/main/java/org/example/ormhibernatefinalcw/config/SessionFactoryConfiguration.java
@@ -1,7 +1,9 @@
 package org.example.ormhibernatefinalcw.config;
 
+import org.example.ormhibernatefinalcw.entity.Admin;
 import org.example.ormhibernatefinalcw.entity.Customer;
 import org.example.ormhibernatefinalcw.entity.Item;
+import org.example.ormhibernatefinalcw.entity.Riciepion;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
@@ -14,7 +16,9 @@ public class SessionFactoryConfiguration {
         Configuration configuration = new Configuration();
         configuration.configure();
         configuration.addAnnotatedClass(Customer.class)
-                .addAnnotatedClass(Item.class);
+                .addAnnotatedClass(Item.class)
+                .addAnnotatedClass(Admin.class)
+                .addAnnotatedClass(Riciepion.class);
         sessionFactory = configuration.buildSessionFactory();
     }
 
---
+++ src/main/java/org/example/ormhibernatefinalcw/entity/Riciepion.java
@@ -0,0 +1,15 @@
+package org.example.ormhibernatefinalcw.entity;
+
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Riciepion {
+    private String email;
+    private String password;
+}
