e12c5b94d3db4c834b81317d82853f7545d1f817
ireshdilsh <ireshdilshan46@gmail.com>
2025-03-26 13:55:29 +0530

    add ProgrammeDto class with fields and constructors

---
+++ src/main/java/org/example/ormhibernatefinalcw/dto/ProgrammeDto.java
@@ -0,0 +1,21 @@
+package org.example.ormhibernatefinalcw.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class ProgrammeDto {
+    private int id;
+    private String name;
+    private String duration;
+    private Double fees;
+
+    public ProgrammeDto(String name, String duration, Double fees) {
+        this.name = name;
+        this.duration = duration;
+        this.fees = fees;
+    }
+}
